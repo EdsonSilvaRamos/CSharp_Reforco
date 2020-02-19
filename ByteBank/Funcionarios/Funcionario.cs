d4f78d3b01743750b11a312562bc8fc93df23373
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2020-02-18 21:14:34 -0300

    Herança/Interface - Aula 1 - Atividade 4

--- ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -1,29 +1,13 @@
 ﻿namespace ByteBank.Funcionarios
 {
     public class Funcionario
-    {
-        // 0 - Funcionario
-        // 1 - Diretor
-        // 2 - Designer
-        // 3 - Coordenador
-        // 4 - Gerente de contas
-        // N - ...
-        private int _tipo;
+    {        
         public string Nome { get; set; }
         public string CPF { get; set; }
-        public double Salario { get; set; }
-
-        public Funcionario(int tipo)
-        {
-            this._tipo = tipo;
-        }
+        public double Salario { get; set; }      
 
         public double GetBonificacao()
-        {
-            if (_tipo == 1)
-            {
-                return this.Salario;
-            }
+        {            
             return this.Salario * 0.10;
         }
     }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -7,14 +7,31 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            var carlos = new Funcionario(1);
+            var gerenciador = new GerenciadorBonificacao();
 
+            var carlos = new Funcionario();
+            
             carlos.Nome = "Carlos";
             carlos.CPF = "123.456.789-50";
             carlos.Salario = 2000;
 
+            gerenciador.Registrar(carlos);
+
+            var roberta = new Diretor();
+            
+            roberta.Nome = "Roberta";
+            roberta.Salario = 5000;
+            
+            gerenciador.Registrar(roberta);
+
+
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }
