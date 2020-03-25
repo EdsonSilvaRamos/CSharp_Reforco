1ce15af22e65d33597941b4f9f20064c0a98188e
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2020-03-24 21:29:41 -0300

    Entendendo exceções - Aula 1, Atividade 7: Blocos Try Catch

--- Parte04_ByteBank/Program.cs
+++ Parte04_ByteBank/Program.cs
@@ -7,11 +7,51 @@ namespace Parte04_ByteBank
     {
         public static void Main(string[] args)
         {
-            var conta = new ContaCorrente(123, 12345);
+            //var conta = new ContaCorrente(123, 12345);
+            //Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
 
-            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Ocorreu um erro!");
+            }
+            
 
             Console.ReadLine();
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não pode ocorrer uma divisão por 0");
+            }
+            
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+
+            return numero / divisor;
         }
     }
 }
