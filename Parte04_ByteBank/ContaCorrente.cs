14b0c3201abc2c7c3af3ab9c6348a5c892b1059d
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2020-03-25 20:21:32 -0300

    Entendendo exceções - Aula 3, Atividade 2: Propriedades somente leitura

--- Parte04_ByteBank/ContaCorrente.cs
+++ Parte04_ByteBank/ContaCorrente.cs
@@ -8,25 +8,9 @@
 
         public static int TotalDeContasCriadas { get; private set; }
 
-
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
+                
+        public int Agencia { get; }
+        public int Numero { get; }
 
         private double _saldo = 100;
 
