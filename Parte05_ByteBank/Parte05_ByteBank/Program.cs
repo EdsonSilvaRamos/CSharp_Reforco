a9e2a198884464b7d64052c639145959e6e1705b
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2020-04-01 22:07:23 -0300

    C# parte 5: Bibliotecas DLLs, documentação e usando o NuGet - Aula 3, Atividade 2: Referencias de DLL

---
+++ Parte05_ByteBank/Parte05_ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank;
+using System;
+
+namespace Parte05_ByteBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conta = new ContaCorrente(123, 1234);
+
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+
+        }
+    }
+}
